6f28e14f4602e5de381bc217f8f2923d706e5d79
FrankLin23 <linzhangsheng23@163.com>
2023-02-14 14:13:21 +0800

    Init Project

---
+++ src/main/java/com/richcapital/forcemusicserver/ForceMusicServerApplication.java
@@ -0,0 +1,13 @@
+package com.richcapital.forcemusicserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForceMusicServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForceMusicServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/richcapital/forcemusicserver/ForceMusicServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richcapital.forcemusicserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForceMusicServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
